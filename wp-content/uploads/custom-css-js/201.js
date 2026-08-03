751129d7f8abb26ffa25d6e4465ff231e1b0e3dd
albina_biriukova <Alja.birjukova@gmail.com>
2026-08-03 11:24:45 +0200

    push all filles

---
+++ wp-content/uploads/custom-css-js/201.js
@@ -0,0 +1,9 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+document.addEventListener("DOMContentLoaded", function() {
+  const yearSpan = document.getElementById("current-year");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});</script>
+<!-- end Simple Custom CSS and JS -->
